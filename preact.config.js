a03b284697cf05655e32088872f6fb754fd1d9a6
David Kolosowski <David.Kolosowski@halfords.co.uk>
2023-07-17 14:07:53 +0100

    more build fixes

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,18 @@
-export default function (config, env, helpers) {
-  if ( env.production ) {
-    config.output.publicPath = '/terrain-builder/';
-  }
-}
+export default {
+  webpack(config, env, helpers, options) {
+
+    const publicPath = process.env.GITHUB_PAGES
+      ? `/${process.env.GITHUB_PAGES}/`
+      : '/';
+    const ghEnv = process.env.GITHUB_PAGES
+      && JSON.stringify(`${process.env.GITHUB_PAGES}`);
+
+    config.output.publicPath = publicPath;
+    const { plugin } = helpers.getPluginsByName(config, 'DefinePlugin')[0];
+    Object.assign(
+      plugin.definitions,
+      { ['process.env.GITHUB_PAGES']: ghEnv }
+    );
+
+  },
+};
